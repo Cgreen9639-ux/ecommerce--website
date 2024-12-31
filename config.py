27ef2a751aac5ed59d8d51f94c174632663a07b9
Cameron Greene <125625082+Cgreen9639-ux@users.noreply.github.com>
2024-12-31 09:39:32 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///store.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY') or 'your_stripe_secret_key'
+    PAYPAL_CLIENT_ID = os.environ.get('PAYPAL_CLIENT_ID') or 'your_paypal_client_id'
+    PAYPAL_SECRET = os.environ.get('PAYPAL_SECRET') or 'your_paypal_secret'

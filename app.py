727d66dbc1fb17e4d5dc6ad129d3c3680218ea22
Cameron Greene <125625082+Cgreen9639-ux@users.noreply.github.com>
2025-01-14 09:57:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,17 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
